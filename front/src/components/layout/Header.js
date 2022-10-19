f74ce5a0f9aa2875629bce9958839fe19c85e27d
CristianGarci42022 <cristiangarci42o22@gmail.com>
2022-10-19 15:22:03 -0500

    Primer producto en mi Fron-end
    Se modifico la parte superior del menu y se le agrego el primer producto de muestra, adicionalmente se adjunto las imagenes que se van a utilizar de cada uno de los productos

--- front/src/components/layout/Header.js
+++ front/src/components/layout/Header.js
@@ -1,39 +1,41 @@
 import React, { Fragment } from 'react'
 
 const Header = () => {
-  return (
-    <Fragment>
-        <nav className='navbar row'>
-            <div className='col-12 col-md-3'>
-                <div className='navbar-brand'>
-                    <img src='./images/LogoCigarreriandCerveceria.png' alt='Cigarrería y Cerveceía'></img>
+    return (
+        <Fragment>
+            <nav className='navbar row'>
+                <div className='col-12 col-md-3'>
+                    <div className='navbar-brand'>
+                        <img src='./images/Logo-Icono/LogoCigarreriandCerveceria.png' alt='Cigarrería y Cerveceía'></img>
+                    </div>
                 </div>
-            </div>
 
-            <div className='col-12 col-md-6 mt-2 mt-md-0'>
-                <div className='input-group'>
-                    <input 
-                        type="text"
-                        id="search-field"
-                        class="form-control"
-                        placeholder='¿Que estás buscando?'></input>
+                <div className='col-12 col-md-6 mt-2 mt-md-0'>
+                    <div className='input-group'>
+                        <input
+                            type="text"
+                            id="search-field"
+                            class="form-control"
+                            placeholder='¿Que estás buscando?'></input>
                         <div class="input-group-append">
                             <button id="search-btn" class="btn">
-                                <i class="fa fa-search" aria-hidden="true">Buscar</i>
+                                <i class="fa fa-search fa-1x text-white" aria-hidden="true"></i>
                             </button>
-                        
-                        </div>
-                </div>
-            </div>
-            <div className='col-12 col-md-3 mt-4 mt-md-0 text-center'>
-                <button className='btn' id='login-btn'>Iniciar Sesión</button>
-                <span id="cart" className='ml-3'>Carrito</span>
-                <span className='ml-1' id="cart_count">2</span> 
-            </div>
-        </nav>
 
-    </Fragment>
-  )
+                        </div>
+                    </div>
+                </div>
+                <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
+                    <span><button className='btn' id="login_btn">Inicie Sesión</button></span>
+                    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                    <span class="ml-1 " id="cart_count">2</span>
+                    <img  src="./images/Logo-Icono/carro3.png" aria-hidden="false"></img>
+                    
+                </div>
+            </nav>
+
+        </Fragment>
+    )
 }
 
 export default Header
