e7f653b5a7ecde9b52ff9d73aff90114e072cbf6
CristianGarci42022 <cristiangarci42o22@gmail.com>
2022-10-19 21:20:49 -0500

    Pre-Diseño de Carro de Compras
    Se rediseño la interfas de home se establecieron los enlaces en la misma ruta de localHost

--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -1,36 +1,138 @@
 import React, { Fragment } from 'react'
 
 export const Home = () => {
-  return (
-    <Fragment>
-        <h1 id="encabezado_productos">Ultimos Productos</h1>
+    return (
+        <Fragment>
+            <h1 id="encabezado_productos">Ultimos Productos</h1>
+            <section id="productos" className='container mt-5'>
+                <div className='row'>
+                    {/*Producto 1*/}
+                    {/* Inicio de tarjeta */}
+                    <div className='col-sm-12 col-md-6 col-lg-3 my-3'>
+                        <div className='card p-3 rounded'>
+                            {/* imagen */}
+                            <img className='card-img-top mx-auto' src='./images/Galgerias/DeTodito.jpg' alt="Detodito Familiar"></img>
+                            {/* Titulo */}
+                            <div className='card-body d-flex flex-column'>
 
-        <section id="productos" className='container mt-5'>
-            <div className='row'>
-                <div className='col-sm-12 col-md-6 col-lg-3 my-3'>
-                    <div className='card p-3 rounded'>
-                        <img className='card-img-top mx-auto' src='./images/Galgerias/DeTodito.jpg' alt="DeTodito"></img>
-                        <div className='card-body d-flex flex-column'>
-                            <h5 id="titulo_producto"><a href='#'>DeTodito Familiar</a></h5>
-                            <div className='rating mt-auto'>
-                                <div className='rating-outer'>
-                                    <div className='rating-inner'></div>
+                                <h5 className="card-title"><a href='http://localhost:3000'>Detodito Familiar</a></h5>
+                                {/* Valoracion de estrellas */}
+                                <div className='rating mt-auto'>
+                                    <div className='rating-outer'>
+                                        <div className='rating-inner'></div>
+                                    </div>
+                                    <span id="No_de_opiniones"> 12 Opiniones</span>
                                 </div>
-                                <span id="No_de_opiniones"> 5 reviews</span>
+                                {/* Precio */}
+                                <div className="card-footer d-flex justify-content-between">
+
+                                    <p className='text-danger font-weight-bold'>$12.600</p>
+                                    <a href='http://localhost:3000'
+                                        id="cart"
+                                        className='btn btn-warning px-3 ml-4'>
+                                        <i className="fa fa-shopping-cart"></i>
+                                    </a>
+                                    {/* Boton de agregar al carrito */}
+                                    {/*
+                                    <p className='text-danger font-weight-bold'>$12.600</p>
+                                    <form href="http://localhost:3000">
+                                        <button
+                                            id="cart"
+                                            type="button"
+                                            className="btn btn-warning px-3 ml-4">
+                                            <i className="fa fa-shopping-cart"></i>
+                                        </button>
+                                    </form>*/}
+                                </div>
                             </div>
-                            <p className='card-text'>$12.000</p><a href='#' id="ver_producto" className='btn btn-block'>
-                                Ver detalle
-                            </a>
+                        </div>
+                    </div>
+                    {/* Fin de tarjeta */}
+
+                    {/*Producto 2*/}
+                    <div className='col-sm-12 col-md-6 col-lg-3 my-3'>
+                        <div className='card p-3 rounded'>
+                            <img className='card-img-top mx-auto' src='./images/Galgerias/Doritos.jpg' alt="Doritos Familiar"></img>
+                            <div className='card-body d-flex flex-column'>
+                                <h5 className="card-title"><a href='http://localhost:3000'>Doritos Familiar</a></h5>
+                                <div className='rating mt-auto'>
+                                    <div className='rating-outer'>
+                                        <div className='rating-inner'></div>
+                                    </div>
+                                    <span id="No_de_opiniones"> 4 Opiniones</span>
+                                </div>
+                                <div className="card-footer d-flex justify-content-between">
+                                    <p className='text-danger font-weight-bold'>$8.600</p>
+                                    <form action="http://localhost:3000/">
+                                        <button
+                                            id="cart"
+                                            type="button"
+                                            className="btn btn-warning px-3 ml-4">
+                                            <i className="fa fa-shopping-cart"></i>
+                                        </button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/*Producto 3*/}
+                    <div className='col-sm-12 col-md-6 col-lg-3 my-3'>
+                        <div className='card p-3 rounded'>
+                            <img className='card-img-top mx-auto' src='./images/Galgerias/M&M.jpg' alt="M&M "></img>
+                            <div className='card-body d-flex flex-column'>
+                                <h5 className="card-title"><a href='http://localhost:3000'>M&M Tradicional</a></h5>
+                                <div className='rating mt-auto'>
+                                    <div className='rating-outer'>
+                                        <div className='rating-inner'></div>
+                                    </div>
+                                    <span id="No_de_opiniones"> 42 Opiniones</span>
+                                </div>
+                                <div className="card-footer d-flex justify-content-between">
+                                    <p className='text-danger font-weight-bold'>$4.600</p>
+                                    <form action="http://localhost:3000/">
+                                        <button
+                                            id="cart"
+                                            type="button"
+                                            className="btn btn-warning px-3 ml-4">
+                                            <i className="fa fa-shopping-cart"></i>
+                                        </button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/*Producto 4*/}
+                    <div className='col-sm-12 col-md-6 col-lg-3 my-3'>
+                        <div className='card p-3 rounded'>
+                            <img className='card-img-top mx-auto' src='./images/Galgerias/TodoRico.jpg' alt="Todo Rico Familiar"></img>
+                            <div className='card-body d-flex flex-column'>
+                                <h5 className="card-title"><a href='http://localhost:3000'>Todo Rico Familiar</a></h5>
+                                <div className='rating mt-auto'>
+                                    <div className='rating-outer'>
+                                        <div className='rating-inner'></div>
+                                    </div>
+                                    <span id="No_de_opiniones"> 20 Opiniones</span>
+                                </div>
+                                <div className="card-footer d-flex justify-content-between">
+                                    <p className='text-danger font-weight-bold'>$18.600</p>
+                                    <form action="http://localhost:3000/">
+                                        <button
+                                            id="cart"
+                                            type="button"
+                                            className="btn btn-warning px-3 ml-4">
+                                            <i className="fa fa-shopping-cart"></i>
+                                        </button>
+                                    </form>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </section>
-        
-    
-    
-    
-    </Fragment>
-  )
+            </section>
+
+        </Fragment>
+    )
 }
 export default Home
--- front/src/components/layout/Header.js
+++ front/src/components/layout/Header.js
@@ -10,32 +10,31 @@ const Header = () => {
                     </div>
                 </div>
 
-                <div className='col-12 col-md-6 mt-2 mt-md-0'>
-                    <div className='input-group'>
-                        <input
-                            type="text"
-                            id="search-field"
-                            class="form-control"
-                            placeholder='¿Que estás buscando?'></input>
-                        <div class="input-group-append">
-                            <button id="search-btn" class="btn">
-                                <i class="fa fa-search fa-1x text-white" aria-hidden="true"></i>
-                            </button>
-
-                        </div>
-                    </div>
-                </div>
-                <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
-                    <span><button className='btn' id="login_btn">Inicie Sesión</button></span>
-                    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-                    <span class="ml-1 " id="cart_count">2</span>
+                <div className="col-12 col-md-6 mt-2 mt-md-0">
+					<div className="input-group">
+						<input
+							type="text"
+							id="search_field"
+							className="form-control"
+							placeholder="Que desea buscar..."
+						/>
+						<div className="input-group-append">
+							<button id="search_btn" className="btn">
+								<i className="fa fa-search"></i>
+							</button>
+						</div>
+					</div>
+				</div>
+                <div className="col-12 col-md-3 mt-4 mt-md-0 text-center text-white ">
+                <span className="ml-1 " id="cart_count">2</span>
                     <img  src="./images/Logo-Icono/carro3.png" aria-hidden="false"></img>
-                    
+                
+                    &nbsp; &nbsp; &nbsp;&nbsp;
+                    <button className="btn btn-success px-4 text-white login-header-btn float-right">Iniciar de sesión</button>
                 </div>
             </nav>
 
         </Fragment>
     )
 }
-
 export default Header
